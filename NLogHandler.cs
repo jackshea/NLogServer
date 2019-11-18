b8cfad4d0351a468d2ea21d11fe81d9ecd01aaec
谢在奎 <jackshea@163.com>
2019-11-19 01:00:46 +0800

    DotNetty + NLog

---
+++ NLogHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using DotNetty.Buffers;
+using DotNetty.Transport.Channels;
+using Newtonsoft.Json;
+using NLog;
+
+namespace NLogServer
+{
+    public class NLogHandler : ChannelHandlerAdapter
+    {
+        public override void ChannelRead(IChannelHandlerContext context, object message)
+        {
+            var Logger = LogManager.GetLogger(context.Name);
+            var buffer = message as IByteBuffer;
+            if (buffer != null)
+            {
+                var rawString = buffer.ToString(Encoding.UTF8);
+                var logMessage = JsonConvert.DeserializeObject<LogMessage>(rawString);
+                Logger.Log(logMessage.LogLevel, logMessage.Message);
+            }
+        }
+
+        public override void ChannelReadComplete(IChannelHandlerContext context) => context.Flush();
+
+        public override void ExceptionCaught(IChannelHandlerContext context, Exception exception)
+        {
+            Console.WriteLine("Exception: " + exception);
+            context.CloseAsync();
+        }
+    }
+
+    public class LogMessage
+    {
+        public LogLevel LogLevel;
+        public string Message;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using DotNetty.Codecs;
+using DotNetty.Handlers.Logging;
+using DotNetty.Handlers.Tls;
+using DotNetty.Transport.Bootstrapping;
+using DotNetty.Transport.Channels;
+using DotNetty.Transport.Channels.Sockets;
+using NLog;
+
+namespace NLogServer
+{
+
+    class Program
+    {
+        static async Task RunServerAsync()
+        {
+            const int Port = 2109;
+
+            IEventLoopGroup bossGroup;
+            IEventLoopGroup workerGroup;
+
+            bossGroup = new MultithreadEventLoopGroup(1);
+            workerGroup = new MultithreadEventLoopGroup();
+
+            var logger = LogManager.GetCurrentClassLogger();
+
+            logger.Info("Test Nlog level.");
+            logger.Trace("Trace");
+            logger.Debug("Debug");
+            logger.Info("Info");
+            logger.Warn("Warn");
+            logger.Error("Error");
+            logger.Fatal("Fatal");
+
+            logger.Info("NLog server starting...");
+            logger.Info($"listening port : {Port}");
+
+
+            try
+            {
+                var bootstrap = new ServerBootstrap();
+                bootstrap.Group(bossGroup, workerGroup);
+                bootstrap.Channel<TcpServerSocketChannel>();
+
+                bootstrap
+                    .Option(ChannelOption.SoBacklog, 100)
+                    .Handler(new LoggingHandler("SRV-LSTN"))
+                    .ChildHandler(new ActionChannelInitializer<IChannel>(channel =>
+                    {
+                        IChannelPipeline pipeline = channel.Pipeline;
+                        pipeline.AddLast(new LoggingHandler("SRV-CONN"));
+                        pipeline.AddLast("framing-enc", new LengthFieldPrepender(2));
+                        pipeline.AddLast("framing-dec", new LengthFieldBasedFrameDecoder(ushort.MaxValue, 0, 2, 0, 2));
+
+                        pipeline.AddLast("nlog", new NLogHandler());
+                    }));
+
+                IChannel boundChannel = await bootstrap.BindAsync(Port);
+
+                Console.ReadLine();
+
+                await boundChannel.CloseAsync();
+            }
+            finally
+            {
+                await Task.WhenAll(
+                    bossGroup.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1)),
+                    workerGroup.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1)));
+            }
+        }
+
+        static void Main() => RunServerAsync().Wait();
+    }
+}
